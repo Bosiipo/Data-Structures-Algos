e22a311a8ba70363e05a007019621ffe8f2927b6
Bosiipo <bosiposhogbolu@gmail.com>
2021-02-21 12:01:07 +0100

    shoot

--- Leetcode questions/LinkedList/reverse-linked-list.js
+++ Leetcode questions/LinkedList/reverse-linked-list.js
@@ -8,7 +8,6 @@ list.append(5);
 
 const {head} = list;
 
-
 var reverseList = function(head) {
   debugger;
   let prev = null;
@@ -25,5 +24,11 @@ var reverseList = function(head) {
   console.log('prev', prev);
   return prev;
 };
+
+// current = [1, 2, 3, 4, 5];
+// @temp = current.next ==> [2, 3, 4, 5]; (grabs the next head on the list);
+// @current.next = prev ==> [1, null]; (updates pointer of current node in list); /It's like PAUSE here\
+// @prev = current ==> prev becomes a ref as in another object is created which matches [1, null] as in current. 
+// @current = temp ==> current continues it's journey as [2, 3, 4, 5]; /And PLAY here\
 
 reverseList(head);
